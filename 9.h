4892fbfa33cc4fed0ecf8e4e7645d130ed704a3d
AmanMatharukailey <156479758+AmanMatharukailey@users.noreply.github.com>
2024-07-26 17:21:55 -0400

    Add files via upload

---
+++ 9.h
@@ -0,0 +1,12 @@
+<html>
+<body >
+ 
+<font size=44>
+<p align="center" >Buffalo</font> 
+<img src="D:\animals\dimpy\114.png" height=400 width=400 alt=sorry >
+
+<br>
+Buffalo are large members of the Bovidae family. <br>There are two types of buffalo: the African or Cape buffalo and the Asian water buffalo.<br> They are dark gray or black animals that look a lot like bulls.
+</img>
+</body>
+</html>
